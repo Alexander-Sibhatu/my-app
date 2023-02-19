576c70aa808b6597d928dacbb17fc0a42fb37e10
Alexander <sibhatualexander5@gmail.com>
2023-02-19 13:26:27 +0100

    src/App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Counter</div>
+    <div>
+      <h1>Coutn: 0</h1>
+      <button className="btn">+</button>
+      <button className="btn">-</button>
+    </div>
   )
 }
 
